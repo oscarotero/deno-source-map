8ee340fb7578877fe8dff4316f504cf5dba48da1
Oscar Otero <oom@oscarotero.com>
2021-04-17 19:04:39 +0200

    deno fmt

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
 # deno-source-map
+
 Port of mozilla / source-map to deno.
 
 Version [0.6.1](https://github.com/mozilla/source-map/releases/tag/0.6.1).
--- mod.js
+++ mod.js
@@ -3,6 +3,6 @@
  * Licensed under the New BSD license. See LICENSE.txt or:
  * http://opensource.org/licenses/BSD-3-Clause
  */
-export {SourceMapGenerator} from './lib/source-map-generator.js';
-export {SourceMapConsumer} from './lib/source-map-consumer.js';
-export {SourceNode} from './lib/source-node.js';
+export { SourceMapGenerator } from "./lib/source-map-generator.js";
+export { SourceMapConsumer } from "./lib/source-map-consumer.js";
+export { SourceNode } from "./lib/source-node.js";
